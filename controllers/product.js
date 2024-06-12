0cbad31968b7e2d83fc5649172c3e1466324b7a7
Tanmay Suryawanshi <tanmaysuryawanshi123@gmail.com>
2024-06-12 21:05:32 +0530

    models added

--- controllers/product.js
+++ controllers/product.js
@@ -1,5 +1,5 @@
 
-const products = [];
+const Product =require('../model/product')
 exports.getAddProduct=(req, res, next) => {
     res.render('add-product', {
       pageTitle: 'Add Product',
@@ -11,11 +11,12 @@ exports.getAddProduct=(req, res, next) => {
   };
 
 exports.postAddProduct=(req, res, next) => {
-    products.push({ title: req.body.title });
+    const product =new Product(req.body.title);
+    product.save();
     res.redirect('/');
   };
 exports.getProducts=(req, res, next) => {
-   
+   const products=Product.fetchAll((products)=>{
     res.render('shop', {
       prods: products,
       pageTitle: 'Shop',
@@ -24,4 +25,6 @@ exports.getProducts=(req, res, next) => {
       activeShop: true,
       productCSS: true
     });
+   });
+  
   };
